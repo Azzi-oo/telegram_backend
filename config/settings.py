bafca843b90b337a2a8b7340a4edeeb2063ce6c0
Azzi-oo <yanberdin.az@yandex.ru>
2024-10-07 18:33:32 +0500

    add new friends tests

--- config/settings.py
+++ config/settings.py
@@ -83,6 +83,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DATETIME_FORMAT': "%Y-%m-%dT%H:%M:%S",
 }
 
 AUTH_USER_MODEL = 'general.User'
--- general/api/tests/test_users.py
+++ general/api/tests/test_users.py
@@ -1,7 +1,7 @@
 from rest_framework.test import APITestCase
 import json
 from rest_framework import status
-from general.factories import UserFactory
+from general.factories import PostFactory, UserFactory
 from django.contrib.auth.hashers import check_password
 
 from general.models import User
@@ -141,7 +141,7 @@ class UserTestCase(APITestCase):
         self.user.refresh_from_db()
         self.assertEqual(friend not in self.user.friends.all())
 
-    def test_user_add_friend_request_when_non_existent_fiend(self):
+    def test_user_add_friend_request_when_non_existent_friend(self):
         friend = UserFactory()
 
         url = f"{self.url}{friend.pk}/remove_friend/"
@@ -151,3 +151,83 @@ class UserTestCase(APITestCase):
 
         self.user.refresh_from_db()
         self.assertTrue(friend not in self.user.friends.all())
+
+    def test_retrieve_user(self):
+        target_user = UserFactory()
+
+        target_user.friends.add(self.user)
+        target_user.friends.add(UserFactory())
+        target_user.save()
+
+        post_1 = PostFactory(author=target_user, title="Post 1")
+        post_2 = PostFactory(author=target_user, title="Post 2")
+
+        PostFactory.create_batch(10)
+
+        response = self.client.get(
+            path=f"{self.url}{target_user.pk}/",
+            format="json",
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        expected_data = {
+            "id": target_user.pk,
+            "first_name": target_user.first_name,
+            "last_name": target_user.last_name,
+            "email": target_user.email,
+            "is_friend": True,
+            "friend_count": 2,
+            "posts": [
+                {
+                    "id": post_1.pk,
+                    "title": post_1.title,
+                    "body": post_1.body,
+                    "created_at": post_1.created_at.strftime("%Y-%m-%dT%H:%M:%S"),
+                },
+                {
+                    "id": post_2.pk,
+                    "title": post_2.title,
+                    "body": post_2.body,
+                    "created_at": post_2.created_at.strftime("%Y-%m-%dT%H:%M:%S"),
+                },
+            ],
+        }
+        self.assertDictEqual(expected_data, response.data)
+
+    def test_get_user_friends(self):
+        target_user = UserFactory()
+
+        friends = UserFactory.create_batch(3)
+        target_user.friends.set(friends)
+        target_user.save()
+
+        UserFactory.create_batch(5)
+
+        url = f"{self.url}{target_user.pk}/friends/"
+        response = self.client.get(path=url, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data["results"]), 3)
+
+        friend_ids = {user.pk for user in friends}
+        for friend in response.data["results"]:
+            self.assertTrue(friend["id"] in friend_ids)
+
+    def test_get_user_friends_response_data_structure(self):
+        target_user = UserFactory()
+
+        friend = UserFactory()
+
+        target_user.friends.add(friend)
+        target_user.save()
+
+        url = f"{self.url}{target_user.pk}/friends/"
+        response = self.client.get(path=url, format="json")
+        self.assertEqual(len(response.data["results"]), 1)
+
+        expected_data = {
+            "id": friend.pk,
+            "first_name": friend.first_name,
+            "last_name": friend.last_name,
+            "is_friend": False,
+        }
+        self.assertDictEqual(response.data["results"][0], expected_data)
