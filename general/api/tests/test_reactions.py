704b8fe54923977a045434c01e99155f9a3c8728
Azzi-oo <yanberdin.az@yandex.ru>
2024-10-17 23:47:42 +0500

    add new changes in tests for reaction

---
+++ general/api/tests/test_reactions.py
@@ -0,0 +1,86 @@
+from rest_framework.test import APITestCase
+import json
+from rest_framework import status
+from general.factories import ChatFactory, MessageFactory, PostFactory, ReactionFactory, UserFactory
+from django.contrib.auth.hashers import check_password
+
+from general.models import Chat, Message, Post, Reaction, User
+
+
+class ReactionTestCase(APITestCase):
+    def setUp(self):
+        self.user = UserFactory()
+        self.client.force_authenticate(user=self.user)
+        self.post = PostFactory()
+        self.url = "/api/reaction/"
+
+    def test_create_reaction(self):
+        data = {
+            "post": self.post.id,
+            "value": Reaction.Values.SMILE,
+        }
+        response = self.client.post(
+            path=self.url,
+            data=data,
+            format="json",
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        reaction = Reaction.objects.last()
+        self.assertEqual(reaction.author, self.user)
+        self.assertEqual(reaction.post, self.post)
+        self.assertEqual(reaction.value, data["value"])
+
+    def test_pass_other_reaction(self):
+        reaction = ReactionFactory(
+            author=self.user,
+            post=self.post,
+            value=Reaction.Values.SMILE,
+        )
+        data = {
+            "post": self.post.id,
+            "value": Reaction.Values.LAUGH,
+        }
+        response = self.client.post(
+            path=self.url,
+            data=data,
+            format="json",
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        reaction.refresh_from_db()
+        self.assertEqual(reaction.value, data["value"])
+
+    def test_pass_the_same_value(self):
+        reaction = ReactionFactory(
+            author=self.user,
+            post=self.post,
+            value=Reaction.Values.SMILE,
+        )
+        data = {
+            "post": self.post.id,
+            "value": Reaction.Values.SMILE,
+        }
+        response = self.client.post(
+            path=self.url,
+            data=data,
+            format="json",
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        reaction.refresh_from_db()
+        self.assertEqual(reaction.value, None)
+
+    def test_pass_invalid_value(self):
+        data = {
+            "post": self.post.id,
+            "value": "some invalid value",
+        }
+        response = self.client.post(
+            path=self.url,
+            data=data,
+            format="json",
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        self.assertEqual(Reaction.objects.count(), 0)
